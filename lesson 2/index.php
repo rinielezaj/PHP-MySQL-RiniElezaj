e07da33782b14362887f5d88c23aa36b128a8245
rinielezaj <rinielezaj08@gmail.com>
2025-11-12 18:50:44 +0100

    lesson 2

---
+++ lesson 2/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// $name="Gerti";
+
+// $school="Digital School";
+// $var1;
+// var_dump($var1);
+// echo $school;
+
+// $x=120;
+// $y=50;
+// echo $x+$y."<br>";
+// echo $x-$y."<br>";
+// echo $x*$y."<br>";
+// echo $x/$y."<br>";
+
+// $a="Digital";
+// $b=" School";
+// echo $a.$b;
+
+$programming="Programming is not cool";
+
+// echo str_replace("not","very",$programming)."<br>";
+
+// echo str_word_count($programming)."<br>";
+
+// echo strlen($programming);
+
+echo strrev($programming);
+?>
