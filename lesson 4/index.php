42085ddebec8ff4e9ae03093d006699ba216a44e
unknown <Student@digitalschool.local>
2025-11-19 19:04:13 +0100

    lesson 4

---
+++ lesson 4/index.php
@@ -0,0 +1,63 @@
+<?php
+// phpinfo();
+// $x=1.5;
+// echo gettype($x);
+
+// function helloWorld(){
+//     echo "Hello world";
+// }
+
+// helloWorld();
+
+// function sum ($x=2,$y=4){
+//     echo $x+$y;
+// }
+
+// sum();
+
+// function maximum ($x,$y){
+//     if($x>$y){
+//         return $x;
+//     }else{
+//         return $y;
+//     }
+// }
+
+// $a=10;
+// $b=6;
+
+// $test=maximum($a,$b);
+// echo "The max of $a and $b is $test";
+
+$x=10;
+function localVariable(){
+    $y=5;
+
+}
+echo $x;
+
+
+
+// echo $sports[0];
+
+// for($i=0;$i<4;$i++){
+//     echo $sports[$i]."<br>";
+// }
+
+// $sports=['Football','Volleyball','Basketball','Handball','Golf','Tennis'];
+
+// array_push($sports,'Tennis');
+// array_pop($sports);
+// array_unshift($sports,'Tennis');
+// array_shift($sports);
+
+// $output=array_slice($sports,2);
+// $output=array_slice($sports,2,2);
+// $myvalue=[1,2,4,5,6];
+// var_dump(array_sum($myvalue));
+// var_dump($output);
+
+$values=[5,7,9,12,35,79,102];
+
+
+?>
